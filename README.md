417d61584e2fe46a66f3d152640251fceb7fd62a
Tornike <tivanashvili189@gmail.com>
2023-04-26 18:13:58 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ChatAppV2
+ 
---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppV2
+//
+//  Created by tornike on 26.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
